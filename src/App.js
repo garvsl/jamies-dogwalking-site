ef62a9f82ff4d0032c44bcef557847d00a9c1b89
garvsl <garvsehgal7@gmail.com>
2023-04-22 11:44:26 -0400

    implement animate presence

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import ImageCollection from "./components/loadingImages";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import Homepage from "./components/homepage";
 
 function App() {
-  return <div className="App"></div>;
+  const [imagesLoading, setImagesLoading] = useState(true);
+
+  return (
+    <AnimatePresence mode="sync" className="App">
+      {imagesLoading && (
+        <ImageCollection
+          setImagesLoading={(value) => setImagesLoading(value)}
+        />
+      )}
+      {!imagesLoading && <Homepage />}
+    </AnimatePresence>
+  );
 }
 
 export default App;
