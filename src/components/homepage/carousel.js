d1e90dab6dde259a967b82982995c09d3fc0150f
garvsl <garvsehgal7@gmail.com>
2023-04-23 13:59:20 -0400

    change carousel design

--- src/components/homepage/carousel.js
+++ src/components/homepage/carousel.js
@@ -1,35 +1,30 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { useEffect, useState, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import imagesArray from "../../images/imagesHandler";
 
 export default function Carousel() {
-  const [count, setCount] = useState(1);
+  const [width, setWidth] = useState(0);
+  const carouselCover = useRef();
 
-  const handleCountUp = () => {
-    if (count === imagesArray.length - 1) {
-      setCount(1);
-    } else {
-      setCount(count + 1);
-    }
-    console.log(count);
-    // console.log(imagesArray.length);
-  };
+  useEffect(() => {
+    setWidth(
+      carouselCover.current.scrollWidth - carouselCover.current.offsetWidth
+    );
+  }, []);
 
-  const handleCountDown = () => {
-    if (count === 0) {
-      setCount(imagesArray.length - 1);
-    } else {
-      setCount(count - 1);
-    }
-    console.log(count);
-  };
   return (
-    <div className="carouselCover">
-      <button onClick={() => handleCountDown()}>prev</button>
-      <button onClick={() => handleCountUp()}>next</button>
-      <div className="imgCover">
-        <img src={imagesArray[count]} alt="" />
-      </div>
-    </div>
+    <motion.div ref={carouselCover} className="carouselCover">
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: -width }}
+        className="imgCover"
+      >
+        {imagesArray.map((image) => (
+          <motion.div className="individualImg" key={image}>
+            <img src={image} alt="" />
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 }
